f260f1adc222e9d1a994e368965a4ce018853418
LHSang <lhsang64.contact@gmail.com>
2024-02-13 17:20:55 +0700

    added types

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+// 1. Type allows you to create a union or intersection of types, while interface does not.
+// 2. Interface can be merged or augmented using declaration merging, while type cannot.
+// 3. Type can be used with mapped types and conditional types, 
+//      providing more advanced type manipulation capabilities.
+// 4. Interface is more commonly used in the TypeScript community 
+//      and is recommended for most cases, especially when defining the shape of an object.
